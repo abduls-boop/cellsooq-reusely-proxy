745afc016e869aa10017a292cea5473608e957f2
abduls-boop <abdul.s@cellsooq.com>
2025-09-22 01:24:38 -0400

    Update _cors.js

--- api/_cors.js
+++ api/_cors.js
@@ -1,1 +1,19 @@
+// api/_cors.js
+export const corsHeaders = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'GET,OPTIONS',
+  'Access-Control-Allow-Headers': 'Content-Type, X-Tenant-Id, X-API-Key',
+};
+
+export function withCors(handler) {
+  return async (req, res) => {
+    if (req.method === 'OPTIONS') {
+      Object.entries(corsHeaders).forEach(([k, v]) => res.setHeader(k, v));
+      return res.status(200).end();
+    }
+    const out = await handler(req, res);
+    Object.entries(corsHeaders).forEach(([k, v]) => res.setHeader(k, v));
+    return out;
+  };
+}
 
